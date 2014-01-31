0899fdb88fbd114ae01db124c45c39d51747ea3c
Toomore Chiang <toomore0929@gmail.com>
2014-02-01 01:20:43 +0800

    Add 'variants' files.

---
+++ variants.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import csv
+
+
+with open('./variants.csv') as files:
+    csv_files = list(csv.reader(files))
+
+address = '台北市大安區金華街187號東樓204室'
+
+for a, b in csv_files:
+    address = address.replace(a, b)
+
+print address
