1c07e62517dbcbd87e21d5a95444ffab73adc23d
Toomore Chiang <toomore0929@gmail.com>
2014-02-02 12:08:33 +0800

    Add moedict variants.

--- variants.py
+++ variants.py
@@ -2,8 +2,11 @@
 import csv
 
 
-with open('./variants_custom.csv.csv') as files:
+with open('./variants_custom.csv') as files:
     csv_files = list(csv.reader(files))
+
+with open('./variants.csv') as files:
+    csv_files.extend(list(csv.reader(files)))
 
 address = '台北市大安區金華街187號東樓204室'
 
